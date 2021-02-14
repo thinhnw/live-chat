79b7281e2a04070842fc9ca8ab50eb290631bf75
thinhnw <thinhnv0412@gmail.com>
2021-02-14 21:24:41 +0700

    Get current user

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -14,20 +14,4 @@ export default {
 </script>
 
 <style>
-nav {
-    padding: 20px;
-    border-bottom: 1px solid #eee;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-nav p {
-    margin: 2px auto;
-    font-size: 16px;
-    color: #444;
-}
-nav p.email {
-    font-size: 14px;
-    color: #999;
-}
 </style>

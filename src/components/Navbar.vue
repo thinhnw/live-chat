ac9df84cb858cb62e8d0a3dc60d152218dabe40c
thinhnw <thinhnv0412@gmail.com>
2021-02-14 21:02:33 +0700

    Add logout function

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,16 +4,44 @@
             <p>Hey there... display name here</p>
             <p class="email">Currently logged in as... email</p>
         </div>
-        <button>Logout</button>
+        <button @click="handleClick">Logout</button>
     </nav>
 </template>
 
 <script>
-export default {
+import useLogout from '../composables/useLogout'
 
+export default {
+    setup() {
+        const { logout, error } = useLogout()
+
+        const handleClick = async () => {
+            await logout()
+            if (!error.value) {
+                console.log('user logged out');
+            }
+        } 
+
+        return { handleClick }
+    }
 }
 </script>
 
 <style>
-
+nav {
+    padding: 20px;
+    border-bottom: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+nav p {
+    margin: 2px auto;
+    font-size: 16px;
+    color: #444;
+}
+nav p.email {
+    font-size: 14px;
+    color: #999;
+}
 </style>

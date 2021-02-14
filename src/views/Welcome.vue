d17dbe6d91e7b15b6edb160670d468d9ad856a86
thinhnw <thinhnv0412@gmail.com>
2021-02-14 11:37:26 +0700

    Style welcome view

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -20,4 +20,21 @@ export default {
     text-align: center;
     padding: 20px 0;
 }
+.welcome form {
+    width: 300px;
+    margin: 20px auto;
+}
+.welcome label {
+    display: block;
+    margin: 20px 0 10px;
+}
+.welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+}
 </style>

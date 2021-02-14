f0adb23ceb04383b378f664018c9091d086b457b
thinhnw <thinhnv0412@gmail.com>
2021-02-14 11:21:49 +0700

    Setup Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7_YtoLfXaQ8PXy7Hpof_OMCjJEk-UVR0",
+    authDomain: "udemy-vue-firebase-sites-a9631.firebaseapp.com",
+    projectId: "udemy-vue-firebase-sites-a9631",
+    storageBucket: "udemy-vue-firebase-sites-a9631.appspot.com",
+    messagingSenderId: "586973348432",
+    appId: "1:586973348432:web:d45338fa7c0687e1854755"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFiresoter = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFiresoter, timestamp }

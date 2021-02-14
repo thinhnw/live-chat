54be3ab4df4f0302fc5c743c060d2742a71dc28e
thinhnw <thinhnv0412@gmail.com>
2021-02-14 20:52:50 +0700

    Style chat room

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <div>
+            <p>Hey there... display name here</p>
+            <p class="email">Currently logged in as... email</p>
+        </div>
+        <button>Logout</button>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
